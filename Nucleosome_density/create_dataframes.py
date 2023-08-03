430c596cae8ac106cda2de8e59d2793a501cfebe
Estienne Swart <eswart@ag-swart-im4.eb.local>
2023-08-03 17:09:35 +0200

    Split nucleosome density and density difference analyses into two separate scripts.

---
+++ Nucleosome_density/create_dataframes.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from functools import reduce
+
+files = ['ICOPs.merged.lengths.IES.downsampled.htseq-count.txt.gz',
+         'ISWI1.IES_read_counts.txt.gz',
+         'NOWA1.IES_read_counts.txt.gz',
+         'PTCAF.htseq-count.downsampled.txt.gz']
+
+# read all input files in data frames
+dfs = []
+for file in files:
+    dfs.append(pd.read_csv(file, compression = 'gzip', sep=" "))
+    
+# merge the data frames 
+df = reduce(lambda left,right: pd.merge(left,right,on=['IES', 'IES_length'],
+                how='outer'), dfs).fillna(np.nan)
+
+#Read IES retention scores into dataframe
+df_irs = pd.read_csv("ICOP_IRS.tab.gz",sep="\t")
+
+
+#Choose the knockdowns you might want to use as filters
+chosen_irs = df_irs[["ies_name", "ICOP1_2"]]
+
+#Combine IES retention score dataframe together with IES read mapping dataframe:
+df_merge = pd.merge(df, chosen_irs, left_on=["IES"], right_on=["ies_name"], how="inner")
+df2 = df_merge.drop(columns=["ies_name"]).rename(columns={"ICOP1_2": "ICOP1_2-KD"})
+df2.set_index("IES")
+
